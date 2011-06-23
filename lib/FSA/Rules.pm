1ba397cccc26708542918c1d33ee1d2b46ccb7b5
David E. Wheeler <david@justatheory.com>
2011-06-22 23:09:40 -0700

    Fix broken rt.cpan.org email address.

--- lib/FSA/Rules.pm
+++ lib/FSA/Rules.pm
@@ -1440,7 +1440,7 @@ contribute!
 
 Please file bug reports via L<GitHub
 Issues|http://github.com/theory/fsa-rules/issues/> or by sending mail to
-L<bug-FSA-Rules.cpan.org|mailto:bug-FSA-Rules.cpan.org>.
+L<bug-FSA-Rules@rt.cpan.org|mailto:bug-FSA-Rules@rt.cpan.org>.
 
 =head1 Authors
 
